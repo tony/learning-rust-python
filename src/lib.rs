9d2a6f26bd1622fd442e16bf27d557f0eecc7520
Tony Narlock <tony@git-pull.com>
2024-11-27 07:34:54 -0600

    Add basic test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+#[pymodule]
+fn learning_rust(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(add, m)?)?;
+    Ok(())
 }
 
 #[cfg(test)]
